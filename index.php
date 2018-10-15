5a2c80bc0d5ea24b1bbbd6eff6ce1bd43a090d8f
Makino <makino.ren.18@shizuoka.ac.jp>
2018-10-15 15:08:46 +0900

    test

--- index.php
+++ index.php
@@ -38,6 +38,9 @@ foreach ($events as $event) {
   }
   if(preg_match('/地震/',$event->getText())){
     $bot->replyText($event->getReplyToken(),'キーワード「地震」に関する情報を表示します。以下の情報が見つかりました。'."\n".'http://www.jma.go.jp/jp/quake/');
+  }else if(preg_match('/災害情報/',$event->getText())){
+    $bot->replyText($event->getReplyToken(),'キーワード「災害情報」に関する情報を表示します。');
+    replyImageMssage($bot, $event->getReplyToken(), 'https://'.$_SERVER['HTTP_HOST'].'imgs/disaster.jpg','https://'.$_SERVER['HTTP_HOST'].'/imgs/preview.jpg')
   }
 }
 
