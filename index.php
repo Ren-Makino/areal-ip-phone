a84528c0025b9a0fff822e3ce080c26d636468a2
Makino <makino.ren.18@shizuoka.ac.jp>
2018-10-15 14:52:32 +0900

    test]

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ foreach ($events as $event) {
     error_log('Non text message has come');
     continue;
   }
-  if(preg_match('地震',$event->getText())){
+  if(preg_match('地震',$event->getText()){
     $bot->replyText($event->getReplyToken(),'キーワード「地震」に関する情報を表示します。以下の情報が見つかりました。'."\n".'http://www.jma.go.jp/jp/quake/');
   }
 }
