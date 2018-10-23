a7376fbfd139e7ca761c4fae0d8d3f1f4ee83a24
Makino <makino.ren.18@shizuoka.ac.jp>
2018-10-23 11:13:06 +0900

    test

--- index.php
+++ index.php
@@ -50,8 +50,9 @@ foreach ($events as $event) {
   }elseif(preg_match('/被災状況/',$event->getText())){
     replyImageMessage($bot, $event->getReplyToken(), 'https://' . $_SERVER['HTTP_HOST'] . '/imgs/original.jpg', 'https://' . $_SERVER['HTTP_HOST'] . '/imgs/preview.jpg');
     $bot->replyText($event->getReplyToken(),'キーワード「被災状況」に関する情報を表示します。');
+    $bot->replyText($event->getReplyToken(),'キーワード「被災状況」に関する情報を表示します。');
   }elseif(preg_match('/登録/',$event->getText())){
-    
+
   }
 
 }
