7b64d5b3bde6d73d66843186b7a55a27cab678f4
Makino <makino.ren.18@shizuoka.ac.jp>
2018-11-21 20:17:08 +0900

    test

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET
 // LINE Messaging APIがリクエストに付与した署名を取得
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
-$file_name;
+$file_name="000-0000-0000";
 
 $latitude;
 $longituide;
