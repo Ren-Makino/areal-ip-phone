33de7ac0b156e07c056988e7c0dd7eaac21852a3
Makino <makino.ren.18@shizuoka.ac.jp>
2018-11-21 20:29:53 +0900

    test

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 // Composerでインストールしたライブラリを一括読み込み
 require_once __DIR__ . '/vendor/autoload.php';
 
+$file_name='000-0000-0000';
+
 // アクセストークンを使いCurlHTTPClientをインスタンス化
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 // CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
@@ -10,12 +12,6 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET
 // LINE Messaging APIがリクエストに付与した署名を取得
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
-$file_name='000-0000-0000';
-
-$latitude;
-$longituide;
-
-$array=array();
 
 
 // 署名が正当かチェック。正当であればリクエストをパースし配列へ
