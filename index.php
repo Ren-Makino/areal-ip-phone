5d66316455a3f898bf70722753b94f996b3519a4
Makino <makino.ren.18@shizuoka.ac.jp>
2019-01-31 11:32:02 +0900

    test

--- index.php
+++ index.php
@@ -91,7 +91,9 @@ foreach ($events as $event) {
 
           if ( abs($myLocation[0]-$theirLocation[0]) < 0.001 ){
             if( abs($myLocation[1]-$theirLocation[1]) < 0.001 ){
-              replyTextMessage($bot,$event->getReplyToken(),$theirMessage);
+              if( $myId != $value){
+                replyTextMessage($bot,$event->getReplyToken(),$theirMessage);
+              }
             }
           }
 
